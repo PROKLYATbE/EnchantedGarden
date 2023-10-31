c650eb9dbf0f79a20d3acc982933efa4fc4dbfe7
PROKLYATbE <spasibo1911@yandex.ru>
2023-10-31 22:57:24 +0300

    create on click script

---
+++ EnchantedGarden/Assets/Script/OnClick.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick : MonoBehaviour
+{
+    public GameObject obj_name;
+    public static string obj_txt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnMouseDown() {
+        obj_txt = gameObject.name;
+        Destroy (gameObject);
+        Destroy (obj_name);
+    }
+}
