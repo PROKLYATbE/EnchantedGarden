fb11eebdd076c2cde4a59750967ed9774cc1e942
PROKLYATbE <spasibo1911@yandex.ru>
2024-03-29 19:53:34 +0300

    Fill Lvl1-3

--- EnchantedGarden/Assets/Script/AdminMode.cs
+++ EnchantedGarden/Assets/Script/AdminMode.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class AdminMode : MonoBehaviour
