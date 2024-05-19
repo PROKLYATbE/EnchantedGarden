c8954c771cc9cdf249220b2423cd13bc3f73f250
Mikhail <hudih@mail.ru>
2024-05-19 14:08:57 +0300

    Added script for hints

---
+++ EnchantedGarden/Assets/Script/HintScript1.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HintScript : MonoBehaviour
+{
+    [SerializeField] int numHints = 2;
+    public static string parent_name = "Interactive_Item";
+    [SerializeField] List<GameObject> objects;
+    public static System.Random rand = new System.Random();
+
+    void Update()
+    {
+        if (numHints == 0)
+            GetComponent<Button>().interactable = false;
+    }
+
+
+    public void EnableHint()
+    {
+        var obj = objects[rand.Next(0, objects.Count)];
+
+        while (!obj.activeSelf)
+            obj = objects[rand.Next(0, objects.Count)];
+
+        obj.GetComponent<SpriteRenderer>().color = new Color(0, 255, 0);
+        --numHints;
+    }
+
+}
